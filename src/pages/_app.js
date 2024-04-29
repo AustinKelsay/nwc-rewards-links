18fd9ab394e0a7438e243a81c1b79a908b1b7ac1
austinkelsay <austinkelsay@yahoo.com>
2024-04-29 12:29:01 -0500

    added primereact / landing page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/lara-dark-indigo/theme.css';
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PrimeReactProvider>
+      <Component {...pageProps} />
+    </PrimeReactProvider>
+  )
 }
